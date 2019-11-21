91737300ced05eeb45383bc963863785c4d7e80e
sghettknot <57679628+sghettknot@users.noreply.github.com>
2019-11-21 04:15:23 +0000

    4:15 am

--- src/pebble/Test.java
+++ src/pebble/Test.java
@@ -4,12 +4,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Test {
 
+    static List<Integer> blackBagX = new ArrayList<Integer>();
+    static List<Integer> hand = new ArrayList<Integer>();
     public static void main(String[] args) throws FileNotFoundException {
+
+        blackBagX.addAll(Arrays.asList(1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 15, 18, 20, 21, 23, 25, 26, 10, 28, 30, 31, 35, 38, 39, 10, 42, 43, 44, 46, 10, 10, 10, 51, 53, 10, 57, 59, 10, 62, 32, 10, 67, 10, 5));
+        hand.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        int pebbleIndex = ThreadLocalRandom.current().nextInt(0, hand.size());
+        int pebble = hand.get(pebbleIndex);
+        hand.remove(pebbleIndex);
+
+        System.out.println(hand.size());
+        /*
         List<Integer> bag = new ArrayList<Integer>();
         FileReader csvFile = new FileReader(new File("D:\\Comp Sci\\2\\Java\\tests\\pebble\\negative.csv"));
         Scanner csvScanner = new Scanner(csvFile);
@@ -23,6 +37,8 @@ public class Test {
         for (int i = 0; i < bag.size(); i++) {
             System.out.println(bag.get(i));
         }
+
+         */
     }
 
 
--- tests/pebble/PebbleGameTest.java
+++ tests/pebble/PebbleGameTest.java
@@ -1,18 +1,19 @@
 package pebble;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class PebbleGameTest {
+import static org.junit.Assert.assertThat;
 
-    String playerName;
-    int playerIndex;
-    private int sumHand;
+public class PebbleGameTest {
 
     List<Integer> hand = (new ArrayList<>());
     List<Integer> blackBag = (new ArrayList<>());
@@ -25,11 +26,6 @@ public class PebbleGameTest {
 
     class negativePebblesException extends Exception {
         private negativePebblesException(){}
-    }
-
-    @Test
-    public void testPositivePlayers() {
-        //assertEquals("Error: Please enter a positive integer for the number of players!",PebbleGame.Player.getNumOfPlayers());
     }
 
     @Test (expected = FileNotFoundException.class)
@@ -69,6 +65,79 @@ public class PebbleGameTest {
     }
 
 
+    @Test
+    public void testDiscard() {
+
+        hand.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        List<Integer> whiteBag = (new ArrayList<>());
+
+        int pebbleIndex = ThreadLocalRandom.current().nextInt(0, hand.size());
+        int pebble = hand.get(pebbleIndex);
+        hand.remove(pebbleIndex);
+        whiteBag.add(pebble);
+
+        int handSize = hand.size();
+        int bagSize = whiteBag.size();
+
+        assertThat(handSize, is(9));
+        assertThat(bagSize, is(1));
+    }
+
+    @Test
+    public void testDraw() {
+        blackBag.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+        List<Integer> hand = (new ArrayList<>());
+
+        int randomIndex = ThreadLocalRandom.current().nextInt(0, blackBag.size());
+        int pebble = blackBag.get(randomIndex);
+        blackBag.remove(randomIndex);
+        hand.add(pebble);
+
+        int handSize = hand.size();
+        int bagSize = blackBag.size();
+
+        assertThat(handSize, is(1));
+        assertThat(bagSize, is(10));
+    }
+
+    @Test
+    public void testDrawEmpty() {
+        List<Integer> hand = (new ArrayList<>());
+        List<Integer> blackBag = (new ArrayList<>());
+        whiteBag.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+
+        int initialBlackBagSize = blackBag.size();
+        int initialWhiteBagSize = whiteBag.size();
+
+        if (blackBag.size() == 0) {
+            blackBag.addAll(whiteBag);
+            whiteBag.clear();
+        }
+
+        int blackBagSize = blackBag.size();
+        int whiteBagSize = whiteBag.size();
+
+        assertEquals(blackBagSize, initialWhiteBagSize);
+        assertEquals(whiteBagSize, initialBlackBagSize);
+    }
+
+    @Test
+    public void testCheckHand() {
+        hand.addAll(Arrays.asList(50, 10, 5, 3, 2, 8, 1, 1, 16, 4));
+        boolean winnerStatus = false;
+        int sumHand = 0;
+        for (int i=0; i<hand.size(); i++) {
+            sumHand += hand.get(i);
+        }
+        if (!winnerStatus && hand.size() == 10) {
+            if (sumHand == 100) {
+                winnerStatus = true;
+            }
+        }
+
+        assertTrue(winnerStatus);
+    }
+
     /**
      * initialise starting hand
      * @throws InterruptedException
@@ -79,7 +148,7 @@ public class PebbleGameTest {
 
         int currentIndex;
         int currentPebble;
-
+        int sumHand = 0;
         // selecting random bag
         for (int i = 1; i < 23; i++) {
             blackBag.add(i);
